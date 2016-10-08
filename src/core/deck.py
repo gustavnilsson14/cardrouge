efbbdb56b6d0eb301a79be0074312164e6c9190b
Daniel Johansson <krexor@gmail.com>
2016-10-08 15:40:49 +0200

    deck hand discard pile

--- src/core/deck.py
+++ src/core/deck.py
@@ -1,23 +1,38 @@
 import random
 
-class Deck:
+class Card_list:
 
     def __init__(self, cards = []):
-        self.deck = cards
-        print("A new deck was created!")
+        self.list = cards
 
-    def add_top(self, card):
-        self.deck += card
-        print("Added card to top ", self.deck)
-
-    def add_bottom(self, card):
-        self.deck.insert(0, card)
+    def add(self, card):
+        self.list += card
 
     def shuffle(self):
-        random.shuffle(self.deck)
+        random.shuffle(self.list)
 
-    def remove_card(self, index):
-        self.deck.pop(index)
+    def remove(self, index):
+        self.list.pop(index)
+
+    def discard(self):
+        self.list = []
+
+class Deck(Card_list):
+
+    def __init__(self, cards = []):
+        Card_list.__init__(self, cards)
 
     def draw_card(self):
-        return self.deck.pop()
+        return self.list.pop()
+
+class Hand(Card_list):
+
+    def __init__(self, cards = []):
+        Card_list.__init__(self, cards)
+
+    def play_card(self, index):
+        self.list.pop(index)
+
+class Discard_pile(Card_list):
+    def __init__(self, cards = []):
+        Card_list.__init__(self, cards)
--- src/gameobject/item.py
+++ src/gameobject/item.py
@@ -5,5 +5,15 @@ class Card(Item):
     def __init__(self,y):
         Item.__init__(self,y)
 
+
+class Combat_card(Card):
+
+    def __init__(self,y):
+        Card.__init__(self,y)
+
+class Magic_card(Card):
+    def __init__(self,y):
+        Card.__init__(self,y)
+
 class Currency(Item):
     pass
