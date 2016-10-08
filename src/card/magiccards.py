ba40d8cc7ed9d9fa015c9dc7b34786f5a2c956a8
Gustav Nilsson <gustav.nilsson@weightpoint.se>
2016-10-08 17:32:57 +0200

    added files

---
+++ src/card/magiccards.py
@@ -0,0 +1,11 @@
+import item
+
+class Fireball(item.MagicCard):
+
+    def __init__(self,y):
+        item.MagicCard.__init__(self,y)
+
+class SummonChair(item.MagicCard):
+
+    def __init__(self,y):
+        item.MagicCard.__init__(self,y)
